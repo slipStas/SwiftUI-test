104dbac49e89ddb146266d1a26fbae8312f5bcae
slipStas <41761920+slipStas@users.noreply.github.com>
2019-08-10 05:00:16 +0300

    add "UserData" file

---
+++ SwiftUI test/UserData.swift
@@ -0,0 +1,25 @@
+//
+//  UserData.swift
+//  SwiftUI test
+//
+//  Created by Stanislav Slipchenko on 10.08.2019.
+//  Copyright © 2019 Stanislav Slipchenko. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    
+    var showFavoritesOnly = true
+    var landmarks = landmarkData
+    
+}
+
+
+
+
+
+
+
+
